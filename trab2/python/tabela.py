e0b448d95682e6275cedda6bf3644ca309edae10
Jan Segre <jan@vialink.com.br>
2012-07-26 11:45:35 -0300

    Limpando código da tabela.

--- trab2/python/tabela.py
+++ trab2/python/tabela.py
@@ -1,47 +1,30 @@
 from math import pi, e, sin, cos, tan, factorial
 from collections import OrderedDict
-from pprint import *
+from pprint import print_table
 
 pie = pi / e
 
 
-def somatermos(n, termo):
-    return sum(termo(float(i)) for i in xrange(n))
-
-
-def mysin(x, n):
-    return somatermos(n, lambda i: (x ** (2 * i + 1)) * (-1) ** i / factorial(2 * i + 1))
-
-
-def mycos(x, n):
-    return somatermos(n, lambda i: (x ** (2 * i)) * (-1) ** i / factorial(2 * i))
-
-
-def mytan(x, n):
-    return mysin(x, n) / mycos(x, n)
-
-
-def mypi(n):
-    return somatermos(n, lambda i: 4 * (-1) ** i / (2 * i + 1))
-
-
-def mye(n):
-    return somatermos(n, lambda i: 1.0 / factorial(i))
-
-
-def mypie(n):
-    return mypi(n) / mye(n)
-
-
 def tabela(n):
+    somatorio = lambda termo: sum(termo(float(i)) for i in xrange(n))
+    mysin = lambda x: somatorio(lambda i: (x ** (2 * i + 1)) * (-1) ** i / factorial(2 * i + 1))
+    mycos = lambda x: somatorio(lambda i: (x ** (2 * i)) * (-1) ** i / factorial(2 * i))
+    mytan = lambda x: mysin(x) / mycos(x)
+    mypi = somatorio(lambda k: 4 * (-1) ** k / (2 * k + 1))
+    mye = somatorio(lambda k: 1.0 / factorial(k))
+    mypie = mypi / mye
+
+    # calcular valores
     valores = OrderedDict([
-        ('pi', [pi, mypi(n)]),
-        ('e', [e, mye(n)]),
-        ('pi/e', [pie, mypie(n)]),
-        ('sen(pi/e)', [sin(pie), mysin(mypie(n), n)]),
-        ('cos(pi/e)', [cos(pie), mycos(mypie(n), n)]),
-        ('tg(pi/e)', [tan(pie), mytan(mypie(n), n)]),
+        ('pi', [pi, mypi]),
+        ('e', [e, mye]),
+        ('pi/e', [pie, mypie]),
+        ('sen(pi/e)', [sin(pie), mysin(mypie)]),
+        ('cos(pi/e)', [cos(pie), mycos(mypie)]),
+        ('tg(pi/e)', [tan(pie), mytan(mypie)]),
     ])
+
+    # calcular erros
     for v in valores.itervalues():
         ve, va = v[0], v[1]
         eabs = ve - va
@@ -50,11 +33,13 @@ def tabela(n):
         v.append(erel)
     tab = [[key] + vals for key, vals in valores.iteritems()]
     tab.insert(0, ['n={0}'.format(n), 'Ve', 'Va', 'Eabs', 'Erel',])
+
+    # imprimir tabela e uma linha em branco
     print_table(tab)
     print
 
 
 if __name__ == '__main__':
-    # imprimir as tabelas n=3 e n=10
+    # imprimir as tabelas de cada valor da lista
     map(tabela, [3, 10, 30, 85])
 
