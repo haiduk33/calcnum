e2c3678b604a7393dc9185b5c5d53e107a85b66a
Jan Segre <jan@vialink.com.br>
2012-07-26 16:10:50 -0300

    Compactando um pouco o código em ruby.

--- trab2/ruby/tabela.rb
+++ trab2/ruby/tabela.rb
@@ -3,37 +3,14 @@ require 'pprint'
 include Math
 PIE = PI / E
 
-def factorial n
-  n.zero? ? 1 : (1..n).reduce(:*)
-end
-
-def somatorio &termo
-  (0..@n).map(&termo).reduce(:+)
-end
-
-def mysin x
-  somatorio {|i| x ** (2 * i + 1) * (-1) ** i / factorial(2 * i + 2)}
-end
-
-def mycos x
-  somatorio {|i| x ** (2 * i) * (-1) ** i / factorial(2 * i)}
-end
-
-def mytan x
-  mysin(x) / mycos(x)
-end
-
-def mypi
-  somatorio {|k| 4.0 * (-1) ** k / (2.0 * k + 1.0)}
-end
-
-def mye
-  somatorio {|k| 1.0 / factorial(k)}
-end
-
-def mypie
-  mypi / mye
-end
+def factorial(n) n.zero? ? 1 : (1..n).reduce(:*) end
+def somatorio(&termo) (0..@n).map(&termo).reduce(:+) end
+def mysin(x) somatorio {|i| x ** (2 * i + 1) * (-1) ** i / factorial(2 * i + 2)} end
+def mycos(x) somatorio {|i| x ** (2 * i) * (-1) ** i / factorial(2 * i)} end
+def mytan(x) mysin(x) / mycos(x) end
+def mypi; somatorio {|k| 4.0 * (-1) ** k / (2.0 * k + 1.0)} end
+def mye; somatorio {|k| 1.0 / factorial(k)} end
+def mypie; mypi / mye end
 
 tabela = lambda do |n|
   @n = n
