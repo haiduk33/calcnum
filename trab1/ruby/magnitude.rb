606c256c161146363ebad2346ecbaa5f8917af26
Jan Segre <jan@vialink.com.br>
2012-07-25 21:34:29 -0300

    Imprimindo de um jeito melhor.

--- trab1/ruby/magnitude.rb
+++ trab1/ruby/magnitude.rb
@@ -7,6 +7,6 @@ while z > 0 do # Enquanto Z > 0 executar
   z /= 2 # Z <- Z/2
 end # Fim (Enquanto)
 n = (log((log(m) / log(2)).abs + 1) / log(2)).floor # N_1 <- int(log(abs(log(M)/log(2))+1))
-puts "Magnitude: " + m.to_s # Escrever "Magnitude:"; M
-puts "Numero de bits do expoente: " + n.to_s # Escrever "Número de bits do expoente:"; N_1
+puts "Magnitude: #{m}" # Escrever "Magnitude:"; M
+puts "Numero de bits do expoente: #{n}" # Escrever "Número de bits do expoente:"; N_1
 
