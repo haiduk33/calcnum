41e5dad29997b9e57dd7c0289d1fb220a233ce62
Jan Segre <jan@vialink.com.br>
2012-07-26 10:58:50 -0300

    Implementando trabalho 2 em python.

---
+++ trab2/python/pprint.py
@@ -0,0 +1,39 @@
+"""Pretty prints."""
+import sys
+
+
+def format_num(num):
+    """Format a number according to given places.
+    Adds commas, etc."""
+
+    try:
+        return "{0: .8f}".format(float(num))
+
+    except (ValueError, TypeError):
+        return "{0:<}".format(num)
+
+
+def get_max_width(table, index):
+    """Get the maximum width of the given column index"""
+    return max([len(format_num(row[index])) for row in table])
+
+
+def print_table(table, out=sys.stdout):
+    """Prints out a table of data, padded for alignment
+    @param out: Output stream (file-like object)
+    @param table: The table to print. A list of lists.
+    Each row must have the same number of columns. """
+    col_paddings = []
+
+    for i in range(len(table[0])):
+        col_paddings.append(get_max_width(table, i))
+
+    for row in table:
+        # left col
+        print >> out, row[0].ljust(col_paddings[0] + 1),
+        # rest of the cols
+        for i in range(1, len(row)):
+            col = format_num(row[i]).rjust(col_paddings[i] + 2)
+            print >> out, col,
+        print >> out
+
---
+++ trab2/python/tabela.py
@@ -0,0 +1,44 @@
+from math import pi, e, sin, cos, tan, factorial
+from collections import OrderedDict
+from pprint import *
+
+pie = pi / e
+
+
+def mysin(x, n):
+    termo = lambda i: (x ** (2 * i + 1)) * (-1) ** i / factorial(2 * i + 1)
+    return sum(termo(i) for i in xrange(n))
+
+
+def mycos(x, n):
+    termo = lambda i: (x ** (2 * i)) * (-1) ** i / factorial(2 * i)
+    return sum(termo(i) for i in xrange(n))
+
+
+def mytan(x, n):
+    return mysin(x, n) / mycos(x, n)
+
+
+def tabela(n):
+    valores = OrderedDict([
+        ('pi', [pi, pi]),# tem que aproximar pi tambem??
+        ('e', [e, e]),# aproximar e tambem??
+        ('pi/e', [pie, pie]),# aproximar pi/e??
+        ('sen(pi/e)', [sin(pie), mysin(pie, n)]),
+        ('cos(pi/e)', [cos(pie), mycos(pie, n)]),
+        ('tg(pi/e)', [tan(pie), mytan(pie, n)]),
+    ])
+    for v in valores.itervalues():
+        ve, va = v[0], v[1]
+        eabs = ve - va
+        erel = eabs / ve
+        v.append(eabs)
+        v.append(erel)
+    tab = [[key] + vals for key, vals in valores.iteritems()]
+    tab.insert(0, ['n={0}'.format(n), 'Ve', 'Va', 'Eabs', 'Erel',])
+    print_table(tab)
+    print
+
+# imprimir as tabelas n=3 e n=10
+map(tabela, [3, 10])
+
