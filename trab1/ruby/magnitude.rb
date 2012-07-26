b8a02e46b9aa0a01aacfb5c271cf48944050f76f
Jan Segre <jan@vialink.com.br>
2012-07-25 21:24:54 -0300

    Adicionando magnitude em ruby.

---
+++ trab1/ruby/magnitude.rb
@@ -0,0 +1,12 @@
+include Math
+# Algoritmo MAGNITUDE
+
+z = 1.0 # Z <- 1
+while z > 0 do # Enquanto Z > 0 executar
+  m = z # M <- Z
+  z /= 2 # Z <- Z/2
+end # Fim (Enquanto)
+n = (log((log(m) / log(2)).abs + 1) / log(2)).floor # N_1 <- int(log(abs(log(M)/log(2))+1))
+puts "Magnitude: " + m.to_s # Escrever "Magnitude:"; M
+puts "Numero de bits do expoente: " + n.to_s # Escrever "Número de bits do expoente:"; N_1
+
