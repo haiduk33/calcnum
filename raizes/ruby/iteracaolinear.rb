6282da5a9b88517c0128f59d1de2ea2ebccdf0fe
Jan Segre <jan@vialink.com.br>
2012-07-27 22:20:31 -0300

    Melhorando o código.

--- raizes/ruby/iteracaolinear.rb
+++ raizes/ruby/iteracaolinear.rb
@@ -1,20 +1,20 @@
+require "base"
 # Algoritmo ITERAÇÃO LINEAR
 
 def iteracaolinear x0, &g
-  x = g.call(x0)
-  k = 0
-  while ((x - x0).abs > @tol or (g.call(x) - x).abs > @tol) and k <= @nmi do
-    x0 = x
-    x = g.call(x0)
-    k += 1
+  x1 = g.call(x0)
+  @nmi.times do
+    x2 = g.call(x1)
+    return x1 if (x2 - x1).abs < @tol and (x1 - x0).abs < @tol
+    x0, x1 = x1, x2
   end
-  if k > @nmi
-    then puts "Ultrapassou NMI"
-    else puts "RAIZ = #{x}"
-  end
+  return :nmi
 end
 
-@tol = 1.0E-3
+@tol = 1e-3
 @nmi = 100
-iteracaolinear(0.6){|x| Math::exp(x) / 3}
+
+f = lambda{|x| Math::exp(x) / 3}
+
+aplicar :iteracaolinear, 0.6, &f
 
