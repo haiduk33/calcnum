6af2320dd74cbb20fa2082d13d2916fcbce79b44
Jan Segre <jan@vialink.com.br>
2012-08-02 11:21:09 -0300

    Corrigindo condição de retorno das secantes.

--- raizes/ruby/secantes.rb
+++ raizes/ruby/secantes.rb
@@ -6,7 +6,7 @@ def secantes x0, x1, &f
   algoritmo do
     p = f.call(x1) / f.call(x0)
     x = x1 - (x0 - x1) * p / (1.0 - p)
-    return x if (x1 - x0).abs < @tol and f.call(x).abs < @tol
+    return x if (x - x1).abs < @tol and f.call(x).abs < @tol
     x0, x1 = x1, x
   end
 end
