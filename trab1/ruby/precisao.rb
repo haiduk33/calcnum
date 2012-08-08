6419be5e6e8ffaffb6f18393646ee76f0baed82d
Jan Segre <jan@vialink.com.br>
2012-08-08 15:51:47 -0300

    Simplificando programa 1 em ruby.

--- trab1/ruby/precisao.rb
+++ trab1/ruby/precisao.rb
@@ -1,14 +1,14 @@
+# encoding: utf-8
 include Math
-# Algoritmo PRECISÃO
 
-p = 1.0 # P <- 1
-while p + 1.0 > 1.0 do # Enquanto P+1 > 1 executar
-  p /= 2.0 # P <- P/2
-end # Fim (Enquanto)
-p *= 2 # P <- 2*P
-d = log10(p).floor.abs # D <- abs(int(log(P)))
-n = (log(p)/log(2.0)).abs.floor #  N_2 <- int(abs(log(P))/log(2))
-puts "Precisao: #{p}" # Escrever "Precisão:"; P
-puts "Numero de digitos significativos decimais: #{d}" # Escrever "Núm. dígitos significativos decimais:"; D
-puts "Numero de bits da mantissa: #{n}" # Escrever "Núm. bits da mantissa:"; N_2
+p = 1.0
+while p + 1.0 > 1.0 do
+  p /= 2.0
+end
+p *= 2
+d = log10(p).floor.abs
+n = (log(p)/log(2.0)).abs.floor
+puts "Precisao: #{p}"
+puts "Núm. digitos significativos decimais: #{d}"
+puts "Núm. bits da mantissa: #{n}"
 
