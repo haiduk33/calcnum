d5ddf7e0c5d9776887288754bda71167bc22c6cc
Jan Segre <jan@vialink.com.br>
2012-08-11 12:04:58 -0300

    Concertando problemas com utf-8 e acentos

--- trab1/erlang/magnitude.erl
+++ trab1/erlang/magnitude.erl
@@ -7,6 +7,6 @@ magnitude(Z, _) -> magnitude(Z / 2, Z).
 main() ->
     M = magnitude(1,0),
     N = trunc(math:log(abs(math:log(M)/math:log(2)) + 1) / math:log(2)),
-    io:fwrite("Magnitude: ~g~n", [M]),
-    io:fwrite("Numero de bits do expoente: ~b~n", [N]).
+    io:fwrite("~ts: ~g~n", [<<"Magnitude">>, M]),
+    io:fwrite("~ts: ~b~n", [<<"Número de bits do expoente">>, N]).
 
--- trab1/erlang/precisao.erl
+++ trab1/erlang/precisao.erl
@@ -8,7 +8,7 @@ main() ->
     P = precisao(2,0),
     D = abs(trunc(math:log10(P))),
     N = trunc(abs(math:log(P)/math:log(2))),
-    io:fwrite("Precisao: ~g~n", [P]),
-    io:fwrite("Num. digitos significativos decimais: ~b~n", [D]),
-    io:fwrite("Num. bits da mantissa: ~b~n", [N]).
+    io:fwrite("~ts: ~g~n", [<<"Precisão">>, P]),
+    io:fwrite("~ts: ~b~n", [<<"Núm. dígitos significativos decimais">>, D]),
+    io:fwrite("~ts: ~b~n", [<<"Núm. bits da mantissa">>, N]).
 
