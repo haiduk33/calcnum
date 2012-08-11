148449fde54884db59286f4c0155a5adc6ca89d3
Jan Segre <jan@vialink.com.br>
2012-08-11 01:47:54 -0300

    Adicionando base do trabalho 2 em Erlang.

---
+++ trab2/erlang/tabela.erl
@@ -0,0 +1,31 @@
+-module(tabela).
+-export([main/0]).
+-import(math, [pi/0, exp/1, sin/1, cos/1, tan/1]).
+-import(lists, [seq/2, nth/2, append/2, map/2, foldl/3]).
+
+myfold(F, I, N) -> {_, R} = foldl(fun(T, {Z, A}) -> {Z * F(T), A + Z} end, {I, 0}, seq(1, N)), R.
+
+mysin(X, N) -> myfold(fun(K) -> -X * X / (2 * K) / (2 * K + 1) end, X, N).
+mycos(X, N) -> myfold(fun(K) -> -X * X / (2 * K) / (2 * K - 1) end, 1, N).
+mytan(X, N) -> mysin(X, N) / mycos(X, N).
+mypi(N)     -> myfold(fun(K) -> -(2 * K - 1) / (2 * K + 1) end, 4, N).
+mye(N)      -> myfold(fun(K) -> 1 / K end, 1, N).
+
+tabela(N) ->
+    PIE = pi() /exp(1),
+    MYPIE = mypi(N) / mye(N),
+    map(fun(ROW) ->
+        Ve = nth(2, ROW),
+        Va = nth(3, ROW),
+        append(ROW, [Va - Ve, 1 - Ve / Va]) end,
+    [["pi",        pi(),     mypi(N)        ],
+     ["e",         exp(1),   mye(N)         ],
+     ["pi/e",      PIE,      MYPIE          ],
+     ["sin(pi/e)", sin(PIE), mysin(MYPIE, N)],
+     ["cos(pi/e)", cos(PIE), mycos(MYPIE, N)],
+     ["tg(pi/e)",  tan(PIE), mytan(MYPIE, N)]]).
+
+main() ->
+    map(fun tabela/1, [3, 10, 30, 84, 10000]).
+
+
