c7cfc414cd8f63b52decebe9541064da92a30aac
Jan Segre <jan@vialink.com.br>
2012-08-13 20:58:25 -0300

    Implementação mais elegante para o algoritmo EQ2GPQ.

--- trab4/cpp/eq2gpq.h
+++ trab4/cpp/eq2gpq.h
@@ -9,16 +9,9 @@ using std::complex;
 
 // Algoritmo EQ2GPQ
 void eq2gpq(double p, double q, complex<double> *raizes, int &j) {
-  double delta = p*p - 4*q;
-  double r = -p / 2;
-  double im = sqrt(abs(delta)) / 2;
-  if (delta >= 0) {
-    raizes[j++] = r + im;
-    raizes[j++] = r - im;
-  } else {
-    raizes[j++] = r + 1i*im;
-    raizes[j++] = r - 1i*im;
-  }
+  complex<double> delta = p*p - 4*q;
+  raizes[j++] = (-p - sqrt(delta)) / 2.;
+  raizes[j++] = (-p + sqrt(delta)) / 2.;
 }
 
 #endif
