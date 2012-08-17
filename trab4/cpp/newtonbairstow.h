3adc7b85f97150092e0c41e952a04a482d2b847d
Jan Segre <jan@vialink.com.br>
2012-08-17 02:36:31 -0300

    Comentando melhor algoritmo.

--- trab4/cpp/newtonbairstow.h
+++ trab4/cpp/newtonbairstow.h
@@ -10,7 +10,6 @@
 #include "eq2gpq.h"
 
 #define MAXN 20
-//#define LAMEAPROX
 
 using std::complex;
 using std::abs;
@@ -19,10 +18,8 @@ using std::sort;
 double tol;
 int nmi;
 
-namespace {
 bool abscmp(const double &a, const double &b) {
   return abs(a) < abs(b);
-}
 }
 
 // Algoritmo NEWTON-BAIRSTOW(N,A)
@@ -47,18 +44,23 @@ complex<double> *newtonbairstow(int n, double a[n]) {
 
   int raiz_atual = 0;
   while (m > 2) {
-    b[m + 2] = 0;
-    b[m + 1] = 0;
-    c[m + 1] = 0;
-    c[m + 0] = 0;
-    dp = 1;
-    dq = 1;
+    // Resetando os primeiros valores de b e c, bem como dp e dq
+    b[m + 2] = b[m + 1] = c[m + 1] = c[m + 0] = 0;
+    dp = dq = 1;
     k = 0;
     while (abs(dp) + abs(dq) > tol && k <= nmi) {
+      // Computa-se os coeficientes de b e c assim como o resto
+      // que serão aproximadamente b[1], b[2], c[0] e c[1].
+      // Note que é nescessário fazer isso em cada iteração
+      // pois p e q mudam.
       for (int i = m; i >= 0; --i) {
         b[i] = a[i] - p * b[i + 1] - q * b[i + 2];
         c[i] = b[i + 1] - p * c[i + 1] - q * c[i + 2];
       }
+      // Newton-Raphson para duas dimensões
+      // a seguinte operação é a inversão de uma matriz 2x2
+      // primeiro calculamos um termo que será nulo quando não
+      // for inversível, depois o resto da inversão.
       double d = c[1] * c[1] - (c[0] - b[1]) * c[2];
       if (abs(d) > 0) {
         dp = (b[1] * c[1] - b[0] * c[2]) / d;
@@ -66,18 +68,26 @@ complex<double> *newtonbairstow(int n, double a[n]) {
         p = p + dp;
         q = q + dq;
       } else {
+        // nesse caso não é inversível, podemos sair do loop
+        // e avisar que tais p e q não convergíram
         m = 0;
         break;
       }
       ++k;
     }
     if (m != 0) {
+      // Uma vez calculada a aproximação resolvemos a equação
+      // do segundo grau
       eq2gpq(p, q, raizes, raiz_atual);
-      --m;
-      --m;
+      // Esse é um jeito eficiente de incrementar m:
+      --m; --m;
+      // Deve-se atualizar o vetor dos coeficientes com o resultado
+      // da divisão já que encontramos duas raízes.
       for (int i = 0; i <= m; ++i) {
         a[m - i] = b[m + 2 - i];
       }
+      // Novas aproximações de p e q, vale ressaltar que ainda
+      // está sendo utilizado os coeficientes antigos.
       p = v[m] / v[m - 2];
       q = v[m - 1] / v[m - 2];
     } else {
@@ -91,6 +101,8 @@ complex<double> *newtonbairstow(int n, double a[n]) {
     }
   }
   if (m != 0 && k <= nmi) {
+    // Caso restem 3 coeficientes (m=2) temos uma equação do
+    // segundo grau, caso contrário a raiz é imediata
     if (m == 2) {
       eq2gpq(b[3], b[2], raizes, raiz_atual);
     } else {
