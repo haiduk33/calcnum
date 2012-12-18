13ae4a48713c33da9ac33286b0cf3ce3fc978c62
Jan Segre <jan@segre.in>
2012-12-17 22:20:02 -0200

    Adicionando rotina PVI PVC.

--- python/provas/er2012.py
+++ python/provas/er2012.py
@@ -45,18 +45,76 @@ def integral_tripla_qg():
     print 'f0 =', f0
 
 
+def pvi_pvc():
+    """PVI PVC"""
+    f = eval('lambda x, y, z: ' + raw_input('Entre F(x, y, z): '))
+    x0, y0, z0, xn, n = input('Entre X0, Y0, Y\'0, XN, N: ')
+    h = float(xn - x0) / n # float para forçar divisão fracionária
+    x, y, z = x0, y0, z0
+
+    for i in range(0, n):
+        k1 = h * z
+        l1 = h * f(x, y, z)
+        k2 = h * (z + l1)
+        l2 = h * f(x + h, y + k1, z + l1)
+        z = z + (l1 + l2) / 2
+        y = y + (k1 + k2) / 2
+        x = x + h
+
+    yn, zn = y, z
+    n, tol, nmi = input('Entre N, TOL, NMI: ')
+    fy = lambda x, y, z: 0.3 / x - z * z / (y * y)
+    fz = lambda x, y, z: 2 * z / y
+
+    # não precisa calcular h denovo
+    v = [0] * (n + 2) # lista de tamanho n + 2
+    y = [0] * (n + 2) # lista de tamanho n + 2
+
+    for i in range(1, n):
+        y[i] = y0 * i * h * (yn - y0) / (xn - x0)
+    kc, vc = 1, 1
+    k, t = [0] * (n + 1), [0] * (n + 1) # listas de tamanho n + 1
+
+    while kc <= nmi and abs(vc) > tol:
+        for i in range(1, n):
+            x = x0 + i * h
+            z = (y[i + 1] - y[i - 1]) / (2 * h)
+            a = -1 - h * fz(x, y[i], z) / 2
+            b = 2 + h * h * fy(x, y[i], z)
+            c = -1 + h * fz(x, y[i], z) / 2
+            d = y[i - 1] - 2 * y[i] + y[i + 1] - h * h * f(x, y[i], z)
+            den = b - a * t[i - 1]
+            k[i] = (d - a * t[i - 1]) / den
+            t[i] = c / den
+        v[n - 1] = k[n - 1]; vc = v[n - 1]
+        y[n - 1] = y[n - 1] + v[n - 1]
+
+        for i in range(n - 2, 0, -1):
+            v[i] = k[i] - t[i] * v[i + 1]
+            y[i] = y[i] + v[i]
+            if abs(vc) < abs(v[i]):
+                vc = v[i]
+        kc = kc + 1
+
+    for i in range(0, n + 1):
+        print 'y[i] =', y[i]
+
+
+FUNCS = [
+    integral_tripla_qg,
+    pvi_pvc,
+    # adicione mais funcoes aqui
+]
+
+
 if __name__ == '__main__':
     loop = True
     while loop:
         try:
-            funcs = [
-                integral_tripla_qg,
-                # adicione mais funcoes aqui
-            ]
-            for i, f in enumerate(funcs):
+            for i, f in enumerate(FUNCS):
                 print i + 1, '-', f.func_doc
             k = input('Esolha uma rotina: ')
-            funcs[k - 1]()
+            FUNCS[k - 1]()
         except KeyboardInterrupt:
             loop = False
     print
