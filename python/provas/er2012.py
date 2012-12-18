c0072046b7743ae58fe2057866cbdd94819e69fd
Jan Segre <jan@segre.in>
2012-12-18 00:02:08 -0200

    progresso...

--- python/provas/er2012.py
+++ python/provas/er2012.py
@@ -124,11 +124,45 @@ def policar(a, n):
             c = [None] * (n + 1)
             for k in range(1, n + 1):
                 c[k] = b[k][j]
-                b[k][j] - 0
+                b[k][j] = 0
             for k in range(1, n + 1):
                 for l in range(1, n + 1):
                     b[k][j] = b[k][j] + a[k][l] * c[l] # ???
     return p
+
+
+def autovet(n, a, autoval):
+    b = [[None] * (n + 1)] * (n + 1)
+    x = [[None] * (n + 1)] * (n + 1)
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            b[i][j] = a[i][j]
+            if j == i:
+                x[i][j] = 1.0
+            else:
+                x[i][j] = 0.0
+
+    for k in range(1, n):
+        p = 0.0
+        for i in range(1, n + 1):
+            p = p + b[i][i]
+        p = p / k
+
+        for i in range(1, n + 1):
+            b[i][i] = b[i][i] - p
+            for j in range(1, n + 1):
+                x[i][j] = autoval[j] * x[i][j] + b[i][j]
+
+        for i in range(1, n + 1):
+            c = [None] * (n + 1)
+            for j in range(1, n + 1):
+                c[j] = b[j][i]
+                b[j][i] = 0
+
+            for j in range(1, n + 1):
+                for l in range(1, n + 1):
+                    b[j][i] = b[j][i] + a[j][l] * c[l]
+
 
 
 def faddeev_leverrier():
